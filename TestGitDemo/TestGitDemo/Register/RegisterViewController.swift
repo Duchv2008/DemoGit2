4fe8e90349b2653d750fc3b5cde357cf7ddfa3b1
Hà Văn Đức <duchv@newwave.com.vn>
2023-11-08 19:44:23 +0700

    make register

---
+++ TestGitDemo/TestGitDemo/Register/RegisterViewController.swift
@@ -0,0 +1,36 @@
+//
+//  RegisterViewController.swift
+//  TestGitDemo
+//
+//  Created by Hà Văn Đức on 08/11/2023.
+//
+
+import UIKit
+
+class RegisterViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+    
+    func register() {
+        print("Register")
+    }
+    
+    func forgot() {
+        print("ABCD")
+    }
+}
